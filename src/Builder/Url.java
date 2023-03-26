80acc399ef6979a0645d9e031c2da668bcf1aedf
nitin023 <khulbe.nitin@gmail.com>
2023-03-26 14:45:27 +0530

    Builder Pattern

---
+++ src/Builder/Url.java
@@ -0,0 +1,9 @@
+package Builder;
+
+public class Url {
+    private String protocol;
+    private String hostname;
+    private String port;
+    private String pathParam;
+    private String queryParameter;
+}
