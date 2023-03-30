c57d7332637dfd6a1b97b08d396083b1d4351de1
nitin023 <khulbe.nitin@gmail.com>
2023-03-30 21:51:07 +0530

    consistent hash ring

--- src/LoadBalancer/LoadBalancerFactory.java
+++ src/LoadBalancer/LoadBalancerFactory.java
@@ -1,11 +1,11 @@
 package LoadBalancer;
 
 public class LoadBalancerFactory {
-    public LoadBalancer createLoadBalancer(String loadBalType) {
-        return switch (loadBalType) {
-            case "round-robin" -> new RoundRobinLoadBalancer();
-            case "least-connection" -> new LeastConnectionLoadBalancer();
-            default -> new RoutedLoadBalancer() ;
-        };
-    }
+//    public LoadBalancer createLoadBalancer(String loadBalType) {
+//        return switch (loadBalType) {
+//            case "round-robin" -> new RoundRobinLoadBalancer();
+//            case "least-connection" -> new LeastConnectionLoadBalancer();
+//            default -> new RoutedLoadBalancer() ;
+//        };
+//    }
 }
