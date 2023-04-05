884484960c7926cdb5158d07e58c7fe4875e0243
pithadiavishal <pithadiavishal8@gmail.com>
2023-04-06 00:07:26 +0530

    added state design patter: vending machine

---
+++ src/StateDesignPattern/VendingMachine/State.java
@@ -0,0 +1,11 @@
+package StateDesignPattern.VendingMachine;
+
+public interface State {
+    void collectCash(int cash);
+
+    void dispenseChange(String productCode);
+
+    void dispenseItem(String productCode);
+
+    void cancelTransaction();
+}
